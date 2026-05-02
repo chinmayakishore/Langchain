e410af852285748c0c3e980e7bb5dec150c3f88d
Chinmaya K <44795542+chinmayakishore@users.noreply.github.com>
2026-05-02 16:33:08 -0400

    Add files via upload

---
+++ trying_lanchain.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
